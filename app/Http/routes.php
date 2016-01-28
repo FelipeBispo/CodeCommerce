94ed60cc9729f38df4c46fe406378d03241e9185
Felipe Bispo <felipe_fpbs@yahoo.com.br>
2016-01-27 22:23:56 -0200

    Projeto Rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,8 +10,34 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+/* Fase 3 - Rotas
+Route::get('category/{category}', function (\CodeCommerce\Category $category){
+
+    //return $category->name;
+    dd($category);
+
+});*/
+
+Route::group(['prefix'=> 'admin'], function (){
+    Route::get('categories/{id?}',['as'=>'categories', 'uses'=> 'AdminCategoriesController@index',function($id=0){
+        if($id)
+            return Redirect::action('WelcomeController@exemplo');
+        return 'Não possui id';
+    }])->where ('id','[0-9]+');
+
+    Route::get('products/{id?}',['as'=>'products', 'uses'=> 'AdminProductsController@index',function($id=0){
+        if($id)
+            return Redirect::action('WelcomeController@exemplo');
+        return 'Não possui id';
+    }])->where ('id','[0-9]+');
+
+});
+
 Route::get('exemplo', 'WelcomeController@exemplo');
 
-Route::get('admin/categories', 'AdminCategoriesController@index');
+//Route::get('admin/categories', 'AdminCategoriesController@index');
 
-Route::get('admin/products', 'AdminProductsController@index');
+//Route::get('admin/products', 'AdminProductsController@index');
+
+//Fase 4 - CRUD
+Route::get('categories','CategoriesController@index');
